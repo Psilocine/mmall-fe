c8b40a115f64be8a4f7afebda9f5372aae122210
psilocine <binbest@yeah.net>
2017-12-27 15:02:53 +0800

    test nav-side 1227

--- src/page/common/nav-side/index.js
+++ src/page/common/nav-side/index.js
@@ -2,7 +2,7 @@
  * @Author: Rosen
  * @Date:   2017-05-19 17:39:14
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2017-12-27 14:58:49
+ * @Last Modified time: 2017-12-27 15:02:39
  */
 'use strict';
 require('./index.css');
@@ -35,52 +35,52 @@ var navSide = {
   roleIdenDiff() {
     var currentUser = _user.getUserInfo();
     console.log(currentUser);
-    switch (currentUser.role) {
-      // 管理员
-      case "1":
-        this.option.navList.push({
-          name: 'user-check',
-          desc: '用户审核',
-          href: './user-check.html'
-        });
-        break;
+    // switch (currentUser.role) {
+    //   // 管理员
+    //   case "1":
+    //     this.option.navList.push({
+    //       name: 'user-check',
+    //       desc: '用户审核',
+    //       href: './user-check.html'
+    //     });
+    //     break;
 
-        // 普通用户
-      case "0":
-        this.option.navList.push({
-          name: 'user-upgrade',
-          desc: '会员升级',
-          href: './user-upgrade.html'
-        });
-        break;
-        // 批发商
-      case "2":
-        this.option.navList.push({
-          name: 'product-add',
-          desc: '商品添加',
-          href: './product-add.html'
-        }, {
-          name: 'product-manage',
-          desc: '商品管理',
-          href: './product-manage.html'
-        });
-        break;
-        // 实体店
-      case "3":
-        this.option.navList.push({
-          name: 'product-add',
-          desc: '商品添加',
-          href: './product-add.html'
-        }, {
-          name: 'product-manage',
-          desc: '商品管理',
-          href: './product-manage.html'
-        });
-        break;
-      default:
-      console.log("can't find the role code") 
-        break;
-    }
+    //     // 普通用户
+    //   case "0":
+    //     this.option.navList.push({
+    //       name: 'user-upgrade',
+    //       desc: '会员升级',
+    //       href: './user-upgrade.html'
+    //     });
+    //     break;
+    //     // 批发商
+    //   case "2":
+    //     this.option.navList.push({
+    //       name: 'product-add',
+    //       desc: '商品添加',
+    //       href: './product-add.html'
+    //     }, {
+    //       name: 'product-manage',
+    //       desc: '商品管理',
+    //       href: './product-manage.html'
+    //     });
+    //     break;
+    //     // 实体店
+    //   case "3":
+    //     this.option.navList.push({
+    //       name: 'product-add',
+    //       desc: '商品添加',
+    //       href: './product-add.html'
+    //     }, {
+    //       name: 'product-manage',
+    //       desc: '商品管理',
+    //       href: './product-manage.html'
+    //     });
+    //     break;
+    //   default:
+    //   console.log("can't find the role code") 
+    //     break;
+    // }
   },
   // 渲染导航菜单
   renderNav: function () {

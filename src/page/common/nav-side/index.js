3f86b9c6874b3174fc00dd9f9cbd3ea567e24890
psilocine <binbest@yeah.net>
2017-12-27 16:08:12 +0800

    test nav-side 1227

--- src/page/common/nav-side/index.js
+++ src/page/common/nav-side/index.js
@@ -2,7 +2,7 @@
  * @Author: Rosen
  * @Date:   2017-05-19 17:39:14
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2017-12-27 16:01:37
+ * @Last Modified time: 2017-12-27 16:03:12
  */
 'use strict';
 require('./index.css');
@@ -32,19 +32,16 @@ var navSide = {
     // 合并选项  
     $.extend(this.option, option);
     this.roleIdenDiff();
-    this.option.navList.push({
-      name: 'user-center',
-      desc: '嘤嘤嘤',
-      href: './user-center.html'})
     this.renderNav();
   },
   // 不同身份用户侧边栏选项不同
   roleIdenDiff: function () {
+    var _this = this;
     _user.getUserInfo(function (res) {
       switch (res.role) {
         // 管理员
         case "1":
-          this.option.navList.push({
+          _this.option.navList.push({
             name: 'user-check',
             desc: '用户审核',
             href: './user-check.html'
@@ -52,7 +49,7 @@ var navSide = {
           break;
           // 普通用户
         case "0":
-          this.option.navList.push({
+          _this.option.navList.push({
             name: 'user-upgrade',
             desc: '会员升级',
             href: './user-upgrade.html'
@@ -60,7 +57,7 @@ var navSide = {
           break;
           // 批发商
         case "2":
-          this.option.navList.push({
+          _this.option.navList.push({
             name: 'product-add',
             desc: '商品添加',
             href: './product-add.html'
@@ -72,7 +69,7 @@ var navSide = {
           break;
           // 实体店
         case "3":
-          this.option.navList.push({
+          _this.option.navList.push({
             name: 'product-add',
             desc: '商品添加',
             href: './product-add.html'

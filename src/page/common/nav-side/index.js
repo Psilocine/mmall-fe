ec6089c3dbb4ec5acfbc430c7efc32d11e045920
psilocine <binbest@yeah.net>
2017-12-27 15:49:47 +0800

    test nav-side 1227

--- src/page/common/nav-side/index.js
+++ src/page/common/nav-side/index.js
@@ -2,13 +2,65 @@
  * @Author: Rosen
  * @Date:   2017-05-19 17:39:14
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2017-12-27 15:45:10
+ * @Last Modified time: 2017-12-27 15:49:39
  */
 'use strict';
 require('./index.css');
 var _mm = require('util/mm.js');
 var _user = require('service/user-service.js');
 var templateIndex = require('./index.string');
+
+// 不同身份用户侧边栏选项不同
+_user.getUserInfo(function (res) {
+  switch (res.role) {
+    // 管理员
+    case "1":
+    navSide.option.navList.push({
+        name: 'user-check',
+        desc: '用户审核',
+        href: './user-check.html'
+      });
+      break;
+      // 普通用户
+    case "0":
+    navSide.option.navList.push({
+        name: 'user-upgrade',
+        desc: '会员升级',
+        href: './user-upgrade.html'
+      });
+      break;
+      // 批发商
+    case "2":
+    navSide.option.navList.push({
+        name: 'product-add',
+        desc: '商品添加',
+        href: './product-add.html'
+      }, {
+        name: 'product-manage',
+        desc: '商品管理',
+        href: './product-manage.html'
+      });
+      break;
+      // 实体店
+    case "3":
+    navSide.option.navList.push({
+        name: 'product-add',
+        desc: '商品添加',
+        href: './product-add.html'
+      }, {
+        name: 'product-manage',
+        desc: '商品管理',
+        href: './product-manage.html'
+      });
+      break;
+    default:
+      console.log("can't find the role code")
+      break;
+  }
+}, function (errMsg) {
+  _mm.errorTips(errMsg);
+});
+
 // 侧边导航
 var navSide = {
   option: {
@@ -30,59 +82,6 @@ var navSide = {
     $.extend(this.option, option);
     this.renderNav();
   },
-  // 不同身份用户侧边栏选项不同
-  roleIdenDiff: (function () {
-    var _this = this;
-    _user.getUserInfo(function (res) {
-      switch (res.role) {
-        // 管理员
-        case "1":
-          _this.option.navList.push({
-            name: 'user-check',
-            desc: '用户审核',
-            href: './user-check.html'
-          });
-          break;
-          // 普通用户
-        case "0":
-          _this.option.navList.push({
-            name: 'user-upgrade',
-            desc: '会员升级',
-            href: './user-upgrade.html'
-          });
-          break;
-          // 批发商
-        case "2":
-          _this.option.navList.push({
-            name: 'product-add',
-            desc: '商品添加',
-            href: './product-add.html'
-          }, {
-            name: 'product-manage',
-            desc: '商品管理',
-            href: './product-manage.html'
-          });
-          break;
-          // 实体店
-        case "3":
-          _this.option.navList.push({
-            name: 'product-add',
-            desc: '商品添加',
-            href: './product-add.html'
-          }, {
-            name: 'product-manage',
-            desc: '商品管理',
-            href: './product-manage.html'
-          });
-          break;
-        default:
-          console.log("can't find the role code")
-          break;
-      }
-    }, function (errMsg) {
-      _mm.errorTips(errMsg);
-    });
-  })(),
   // 渲染导航菜单
   renderNav: function () {
     // 计算active数据

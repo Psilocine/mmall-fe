0ce30611a4d84593bbae2739fc0c24cbc9ac0116
psilocine <binbest@yeah.net>
2018-01-16 12:23:42 +0800

    fix head-shop shop-name bug 0116

--- src/page/user-shop/index.js
+++ src/page/user-shop/index.js
@@ -2,13 +2,13 @@
  * @Author: PsiloLau 
  * @Date: 2018-01-16 01:42:16 
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2018-01-16 01:53:01
+ * @Last Modified time: 2018-01-16 12:23:17
  */
 'use strict';
 
 require('./index.css');
 require('page/common/nav/index.js');
-require('page/common/header/index.js');
+require('page/common/header-shop/index.js');
 var _mm = require('util/mm.js');
 var _product = require('service/product-service.js');
 var templateIndex = require('./index.string');

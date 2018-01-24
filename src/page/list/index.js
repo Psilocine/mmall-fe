4120a3c83382a0f42218db6a8fa87b84698da54f
psilocine <binbest@yeah.net>
2018-01-24 18:54:11 +0800

    test

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,18 +2,19 @@
  * @Author: Mall
  * @Date:   2017-05-27 17:57:49
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2018-01-24 18:50:31
+ * @Last Modified time: 2018-01-24 18:53:56
  */
 'use strict';
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var tem = require('./123.string');
+var tem = require('./1234.string');
 console.log(tem);
+var templateIndex = require('./123.string');
+console.log(templateIndex);
 var _mm = require('util/mm.js');
 var _product = require('service/product-service.js');
 var Pagination = require('util/pagination/index.js');
-var templateIndex = require('./1234.string');
 
 
 var page = {

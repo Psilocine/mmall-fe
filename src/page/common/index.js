900f081335478bec2a49f968d9a84ec9efe59422
psilocine <binbest@yeah.net>
2018-02-02 16:11:18 +0800

    test 360 promise

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Rosen
 * @Date:   2017-05-09 16:48:55
-* @Last Modified by:   Rosen
-* @Last Modified time: 2017-05-17 21:20:30
+ * @Last Modified by: PsiloLau
+ * @Last Modified time: 2018-02-02 16:11:07
 */
 
 'use strict';
@@ -10,3 +10,5 @@
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
 require('./footer/index.css');
+
+window.Promise = Promise;

55cc434b6ea80e8022cf8ae854250d09fdb4b7e9
psilocine <505256372@qq.com>
2018-04-23 01:19:48 +0800

    modified index css and increase search dist

--- src/page/common/header/index.js
+++ src/page/common/header/index.js
@@ -2,12 +2,16 @@
  * @Author: Rosen
  * @Date:   2017-05-18 19:30:12
  * @Last Modified by: PsiloLau
- * @Last Modified time: 2018-02-13 23:08:32
+ * @Last Modified time: 2018-04-23 01:06:18
  */
 
 'use strict';
 require('./index.css');
 var _mm = require('util/mm.js');
+// 三级select框
+var _dpd = require('util/distpicker/distpicker.data.js');
+var _dp = require('util/distpicker/distpicker.js');
+
 // 通用页面头部
 var header = {
 	init: function () {
@@ -21,30 +25,44 @@ var header = {
 		if (keyword) {
 			$('#search-input').val(keyword);
 		};
+		$('#distpick').distpicker({
+			autoSelect: false
+		});
 	},
 	bindEvent: function () {
 		var _this = this;
 		// 点击搜索按钮以后，做搜索提交
-		$('#search-btn').click( () => this.searchSubmit() );
-		
+		$('#search-btn').click(() => this.searchSubmit());
+
 		// 搜索类型
 		$('.search-item').click(function () {
 			$('#searchType').html($(this).html())
-			$('.search-list').css('visibility','hidden');
+			$('.search-list').css('visibility', 'hidden');
 			$('.search-list').removeAttr('style');
-			
+
 		});
+
+		// 按区域搜索
+		$('.dist-btn').click(function () {
+			if ($(this).html() === '按区域搜索') {
+				$(this).html('取消');
+				$('#distpick').css('display', 'block');
+			} else {
+				$(this).html('按区域搜索');
+				$('#distpick').css('display', 'none');
+			}
+		})
 
 		// 回车提交
 		$('#search-input').keydown(function (e) {
-			if(e.keyCode === 13) {
+			if (e.keyCode === 13) {
 				_this.searchSubmit();
 			}
 		})
 
 		// 点击搜索type 出现搜索list 适配移动端
-		$('.search-type').click(function() {
-			$('.search-list').css('visibility', 'visible');			
+		$('.search-type').click(function () {
+			$('.search-list').css('visibility', 'visible');
 			$('.search-list').removeAttr('style');
 		})
 	},
@@ -62,7 +80,7 @@ var header = {
 			else {
 				_mm.goHome();
 			}
-		} else if(type === '店铺') {
+		} else if (type === '店铺') {
 			if (keyword) {
 				window.location.href = './shop-list.html?keyword=' + keyword;
 			}
